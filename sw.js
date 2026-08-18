3fd633219792514ba5395b167ce31c6b12293898
Pablo672-sys <pabloamorim101@gmail.com>
2026-08-18 20:53:50 -0300

    Adicionar service worker do Loki One

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='loki-one-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./app/']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
