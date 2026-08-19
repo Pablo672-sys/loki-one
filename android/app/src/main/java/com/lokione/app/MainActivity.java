56e70e97b2417e4072ad66c92d75c969ec0416c6
Pablo672-sys <pabloamorim101@gmail.com>
2026-08-18 21:38:58 -0300

    Corrigir cache do APK Android e forçar versão atual do Loki One

--- android/app/src/main/java/com/lokione/app/MainActivity.java
+++ android/app/src/main/java/com/lokione/app/MainActivity.java
@@ -10,14 +10,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         WebView webView = new WebView(this);
         webView.setWebViewClient(new WebViewClient());
+
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(false);
         settings.setBuiltInZoomControls(false);
-        webView.loadUrl("https://pablo672-sys.github.io/loki-one/app/");
+        settings.setDisplayZoomControls(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.loadUrl("https://pablo672-sys.github.io/loki-one/app/?android=1&v=0.5.1");
+
         setContentView(webView);
     }
 }
