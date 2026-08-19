35fcfed0f96fad5763da720c741fad5b3348e620
Pablo672-sys <pabloamorim101@gmail.com>
2026-08-19 13:07:10 -0300

    chore: refresh web cache for Loki One 2.0

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='loki-one-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./app/','./loki-icon.svg','./loki-icon.webp','./manifest.webmanifest']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
+const CACHE='loki-one-v4';
+const ASSETS=['./','./app/','./loki-icon.svg','./loki-icon.webp','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
