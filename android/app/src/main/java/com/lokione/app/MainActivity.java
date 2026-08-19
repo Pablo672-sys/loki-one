94ade5ba41898efd2d47e3f87fdef55b421036eb
Pablo672-sys <pabloamorim101@gmail.com>
2026-08-18 21:54:37 -0300

    Tornar Android independente: carregar Loki One local no APK

--- android/app/src/main/java/com/lokione/app/MainActivity.java
+++ android/app/src/main/java/com/lokione/app/MainActivity.java
@@ -10,23 +10,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         WebView webView = new WebView(this);
         webView.setWebViewClient(new WebViewClient());
-
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(false);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
         settings.setMediaPlaybackRequiresUserGesture(false);
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-
-        webView.clearCache(true);
-        webView.clearHistory();
-        webView.loadUrl("https://pablo672-sys.github.io/loki-one/app/?android=1&v=0.5.1");
-
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
 }
